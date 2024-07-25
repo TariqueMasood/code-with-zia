7338d598ec6ce7ada64078f1e5f47494d1aa17dd
TariqueMasood <64158034+TariqueMasood@users.noreply.github.com>
2024-07-25 12:36:07 +0530

    ECE:Learn-section done

---
+++ src/app/components/header/header.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Link from "next/link";
+import styles from "./header.module.css";
+import { CiMenuFries } from "react-icons/ci";
+import { IoClose } from "react-icons/io5";
+import { useState } from "react";
+import NavList from "../nav-list/nav-list";
+import logo from "../../../../public/images/logo.png";
+import Image from "next/image";
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <header className={styles.header}>
+        <div className="container">
+          <div className={styles.headerWrapper}>
+            <div className={styles.logo}>
+              <Link href="/">
+                <Image width={120} height={40} src={logo} alt="Logo" />
+              </Link>
+            </div>
+            <div
+              onClick={() => setIsOpen(!isOpen)}
+              className={styles.hamburger}
+            >
+              {isOpen ? <IoClose /> : <CiMenuFries />}
+            </div>
+            <nav className={`${styles.navbar} ${isOpen ? styles.active : ""}`}>
+              {<NavList isOpen={isOpen} setIsOpen={setIsOpen} />}
+              <Link href="#" className={styles.quoteBtn}>
+                Get a quote
+              </Link>
+            </nav>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/app/components/learn-section/learn-section.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styles from "../../components/learn-section/learn-section.module.css";
+import { FaArrowCircleRight } from "react-icons/fa";
+
+export const LearnSection = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className="container">
+        <h1 className={styles.title}>What you’ll Learn In this</h1>
+        <p className={styles.subTitle}>
+          4 Hours Live <span>Workshop</span>
+        </p>
+        <div className={styles.list}>
+          <div>
+            <ul>
+              <li>
+                Learn about Data Analytics, Business Analytics and Business
+                Intelligence
+              </li>
+              <li>
+                Learn about the world&rsquo;s top Data Analytics tool by
+                Microsoft POWER BI
+              </li>
+              <li>
+                Learn about industry needs and expectations from 21st-century
+                data analyst
+              </li>
+              <li>5 steps ROADMAP to complete any data analytics project</li>
+              <li>Power BI installation and importing Dataset</li>
+              <li>Creating 10+ Visuals in Power BI</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Data Modelling and Transformation using Power BI</li>
+              <li>End-to-End Interactive reports using Power BI</li>
+              <li>Guidelines on how to show your projects on Linkedin</li>
+              <li>
+                How to make a strong portfolio and How to start freelancing
+              </li>
+              <li>How to prepare an Industry ready resume</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
