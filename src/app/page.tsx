f520e744b46cba79a5c99d050ca438c181ff078c
TariqueMasood <64158034+TariqueMasood@users.noreply.github.com>
2024-08-12 22:56:54 +0530

    CSE:Issue Fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { LearnSection } from "./components/learn-section/learn-section";
 import ScrollBtn from "./components/scroll-btn/scroll-btn";
 import TestimonialCarousel from "./components/testimonial-carousel/testimonial-carousel";
 import { Dashboard } from "./components/dashboard/dashboard";
-import { Testimonial } from "./components/testimonial/testimonial";
 import { Certificate } from "./components/certificate/certificate";
 import AccordionContent from "./components/accordion-content/accordion-content";
 
